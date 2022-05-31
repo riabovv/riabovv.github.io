b5905f054f95bd6b4cdab18b20a40b0a18ef3114
Vadym <flypilott74@gmail.com>
2022-05-31 11:32:28 +0300

    fix: homepage url

--- react-todo-app/service-worker.js
+++ react-todo-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec1720b7948cf23873fb5348dda54ba1.js"
+  "/react-todo-app/precache-manifest.7d9476e3e928ae2c98d6d06a462c40bb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-todo-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
